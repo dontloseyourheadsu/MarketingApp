8a13d0ca86129dc12dc12fbcc1c832134dc0aa10
dontloseyourheadsu <dontloseyourheadsu@gmail.com>
2025-06-11 19:05:29 -0600

    Removed python related files

--- apps/fastapi/schemas/generic.py
+++
@@ -1,9 +0,0 @@
-﻿from datetime import datetime
-from pydantic import BaseModel, Field
-
-class ORMBase(BaseModel):
-    id: int = Field(..., example=1)
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
